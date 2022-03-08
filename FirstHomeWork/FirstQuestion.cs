a0f6b0ca9879b277f243ccb70ec5a58017e89106
jamwitk <hushdilcss@gmail.com>
2022-03-08 20:53:45 +0300

    first commit

---
+++ FirstHomeWork/FirstQuestion.cs
@@ -0,0 +1,46 @@
+﻿namespace FirstHomeWork;
+
+public class FirstQuestion
+{
+    /*
+     * Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+     * Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+     * Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+     */
+    public static void Main(string[] arg)
+    {
+        var girilenSayilar = new List<int>();
+        start:
+        Console.WriteLine("Pozitif sayı giriniz : ");
+        var input = Convert.ToInt16(Console.ReadLine());
+        if (CheckIfPositiveNumber(input))
+        {
+            Console.Clear();
+            goto start;
+        }
+
+        for (var i = 0; i < input; i++)
+        {
+            Console.Clear();
+            checkpoint:
+            Console.WriteLine((i + 1)+". pozitif sayıyı giriniz: ");
+            var _input = Convert.ToInt16(Console.ReadLine());
+            if(CheckIfPositiveNumber(_input))
+            {
+                goto checkpoint;
+                i--;
+            }
+            girilenSayilar.Add(_input);
+        }
+        Console.WriteLine("Çift sayılar");
+        foreach (var t in girilenSayilar.Where(t => t%2==0))
+        {
+            Console.WriteLine(t);
+        }
+    }
+
+    public static bool CheckIfPositiveNumber(int input)
+    {
+        return input <= 0;
+    }
+}
---
+++ SecondHomeWork/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstHomeWork
+{
+    /*Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m).
+     * Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan
+     * m'e eşit yada tam bölünenleri console'a yazdırın.
+     *
+     */
+    public class SecondQuestion
+    {
+        public static void Main(string[] arg)
+        {
+            var girilenSayilar = new List<int>();
+            start:
+            Console.WriteLine("İki pozitif sayı giriniz.");
+            var arr = Console.ReadLine().Split(' ').ToArray();
+            var N = Convert.ToInt16(arr[0]);
+            var M = Convert.ToInt16(arr[1]);
+            if (CheckIfPositiveNumber(N) || CheckIfPositiveNumber(M))
+            {
+                Console.Clear();
+                goto start;
+            }
+            Console.Clear();
+            for (var i = 0; i < N; i++)
+            {
+                checkpoint:
+                Console.WriteLine((i + 1) + ". sayı giriniz.");
+                var input = Convert.ToInt16(Console.ReadLine());
+                if (CheckIfPositiveNumber(input))
+                {
+                    goto checkpoint;
+                    i--;
+                }
+                girilenSayilar.Add(input);
+            }
+            Console.Clear();
+            Console.WriteLine("Sonuç:");
+            foreach (var t in girilenSayilar.Where(t => t == M || t % 2 == 0))
+            {
+                Console.WriteLine(t);
+            }
+        }
+        public static bool CheckIfPositiveNumber(int input)
+        {
+            return input <= 0;
+        }
+    }
+
+}
